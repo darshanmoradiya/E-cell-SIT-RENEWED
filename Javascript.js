a25c327f29f878f5201371abeac558bb5e961447
DarshanMoradiya <70023867+darshanmoradiya@users.noreply.github.com>
2024-04-13 14:46:17 +0530

    edited js and css

--- Javascript.js
+++ Javascript.js
@@ -1,24 +1,24 @@
 /*-----------------BELOW IS PRELOADER-------------*/
 
-// let timer1
-// let preloader=document.querySelector('.preloader')
-// let nav=document.querySelector('.navbar');
-// timer1=setInterval(()=>{
-// preloader.classList.add('endpreloadopacity');
-// setTimeout(timer1);
+let timer1
+let preloader=document.querySelector('.preloader')
+let nav=document.querySelector('.navbar');
+timer1=setInterval(()=>{
+preloader.classList.add('endpreloadopacity');
+setTimeout(timer1);
 
-// },4500)
-// let timer2= setInterval(()=>{
-//     nav.classList.remove('nav_beforepreload');
-//     setTimeout(timer2);
-//     document.body.style.transition="0s none";
-// document.body.style.transition.delay="none";
-// document.querySelector('.headervdo').classList.add('headervdo_afterpreload');
-// },4600)
-// let timer3= setInterval(()=>{
-//     preloader.style.display='none';
-//     setTimeout(timer3);
-// },5500)
+},4500)
+let timer2= setInterval(()=>{
+    nav.classList.remove('nav_beforepreload');
+    setTimeout(timer2);
+    document.body.style.transition="0s none";
+document.body.style.transition.delay="none";
+document.querySelector('.headervdo').classList.add('headervdo_afterpreload');
+},4600)
+let timer3= setInterval(()=>{
+    preloader.style.display='none';
+    setTimeout(timer3);
+},5500)
 
 /*-----------------ABOVE WAS PRELOADER-------------*/
 
@@ -68,25 +68,25 @@ setInterval(()=>{
     if(Math.abs(changedsec-currentsec)%8==1)
     {   /*-----------------BELOW IS PRELOADER-------------*/
 
-// let timer1
-// let preloader=document.querySelector('.preloader')
-// let nav=document.querySelector('.navbar');
-// timer1=setInterval(()=>{
-// preloader.classList.add('endpreloadopacity');
-// setTimeout(timer1);
+let timer1
+let preloader=document.querySelector('.preloader')
+let nav=document.querySelector('.navbar');
+timer1=setInterval(()=>{
+preloader.classList.add('endpreloadopacity');
+setTimeout(timer1);
 
-// },4500)
-// let timer2= setInterval(()=>{
-//     nav.classList.remove('nav_beforepreload');
-//     setTimeout(timer2);
-//     document.body.style.transition="0s none";
-// document.body.style.transition.delay="none";
-// document.querySelector('.headervdo').classList.add('headervdo_afterpreload');
-// },4600)
-// let timer3= setInterval(()=>{
-//     preloader.style.display='none';
-//     setTimeout(timer3);
-// },5500)
+},4500)
+let timer2= setInterval(()=>{
+    nav.classList.remove('nav_beforepreload');
+    setTimeout(timer2);
+    document.body.style.transition="0s none";
+document.body.style.transition.delay="none";
+document.querySelector('.headervdo').classList.add('headervdo_afterpreload');
+},4600)
+let timer3= setInterval(()=>{
+    preloader.style.display='none';
+    setTimeout(timer3);
+},5500)
 
 /*-----------------ABOVE WAS PRELOADER-------------*/
 
